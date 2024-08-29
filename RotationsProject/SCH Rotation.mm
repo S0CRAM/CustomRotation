d401f6c4658c407ef94daf2ecb2bb195813b6864
S0CRAM <m.aguiar0954@gmail.com>
2024-08-29 16:15:33 -0400

    Create SCH Rotation.mm

---
+++ RotationsProject/SCH Rotation.mm
@@ -0,0 +1,558 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="SCH Rotation" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1724880162411">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#002b36" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_solarized_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1664261437" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#002b36" BACKGROUND_COLOR="#fdf6e3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#fdf6e3" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1664261437" STARTINCLINATION="132.6 pt;-31.8 pt;" ENDINCLINATION="132.6 pt;24 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#2aa198" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#fdf6e3" BACKGROUND_COLOR="#d33682" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#d33682"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1940777954" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_1940777954"/>
+<font NAME="Ubuntu" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#002b36" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#fdf6e3" BACKGROUND_COLOR="#073642" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#fdf6e3" BACKGROUND_COLOR="#586e75" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#fdf6e3" BACKGROUND_COLOR="#657b83" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#fdf6e3" BACKGROUND_COLOR="#839496" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#fdf6e3" BACKGROUND_COLOR="#93a1a1" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#eee8d5" BORDER_COLOR_LIKE_EDGE="true">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#fdf6e3" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Heal Actions" POSITION="bottom_or_right" ID="ID_124908995" CREATED="1724880162630" MODIFIED="1724880175922">
+<node TEXT="HealSingleAbility" ID="ID_479942952" CREATED="1724880327258" MODIFIED="1724880332586">
+<node TEXT="Lustrate" ID="ID_1048464188" CREATED="1724880603581" MODIFIED="1724884188837"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      600 Heal
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Excog" ID="ID_1243520696" CREATED="1724880612792" MODIFIED="1724880625093"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      If target HP &lt;= 50%
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Aetherpact" ID="ID_438756303" CREATED="1724882055218" MODIFIED="1724942853427"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      300 Heal
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="HealSingleGCD" ID="ID_1930602194" CREATED="1724880319749" MODIFIED="1724880326305">
+<node TEXT="" ID="ID_91204068" CREATED="1724884440449" MODIFIED="1724884440449">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Adloqium" ID="ID_308608684" CREATED="1724882590273" MODIFIED="1724884434069"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      300 Heal
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="" ID="ID_359387664" CREATED="1724884440448" MODIFIED="1724884440449">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Because its GCD, it&apos;ll be used as heal action" ID="ID_660174931" CREATED="1724884440449" MODIFIED="1724884443585"/>
+</node>
+<node TEXT="Physick" ID="ID_1597407184" CREATED="1724880503644" MODIFIED="1724884162886"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      450 Heal
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="HealAreaAbility" ID="ID_581767062" CREATED="1724880343918" MODIFIED="1724880349488">
+<node TEXT="" ID="ID_1460477136" CREATED="1724884216291" MODIFIED="1724884216291">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Whispering Dawn" ID="ID_71819291" CREATED="1724882402668" MODIFIED="1724884211104"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      80 HoT
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="" ID="ID_703716745" CREATED="1724884216289" MODIFIED="1724884216291">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Can use when AverageHP is &gt;=90%" ID="ID_1674130821" CREATED="1724884216292" MODIFIED="1724884234577"/>
+</node>
+<node TEXT="Fey Blessing" ID="ID_1551649035" CREATED="1724883229055" MODIFIED="1724883246782"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      320 Heal
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Indomitability" ID="ID_77872569" CREATED="1724883729915" MODIFIED="1724884145076"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      400 Heal
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Consolation" ID="ID_101824041" CREATED="1724883291624" MODIFIED="1724883336815"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      250 Heal + Shield
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="HealAreaGCD" ID="ID_586506380" CREATED="1724880350602" MODIFIED="1724880354012">
+<node TEXT="Succor" ID="ID_808340849" CREATED="1724882723405" MODIFIED="1724884726169"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      200 Heal | Check party members for status, don't overlap with other Shield healers
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node TEXT="Defense Actions" POSITION="top_or_left" ID="ID_1854976367" CREATED="1724880178768" MODIFIED="1724880184634">
+<node TEXT="DefenseSingleAbility" ID="ID_1553579729" CREATED="1724880355542" MODIFIED="1724880360891"/>
+<node TEXT="DefenseSingleGCD" ID="ID_1267720852" CREATED="1724880362214" MODIFIED="1724880367653"/>
+<node TEXT="DefenseAreaAbility" ID="ID_1035044818" CREATED="1724880381474" MODIFIED="1724880386301">
+<node TEXT="" ID="ID_123013036" CREATED="1724884489352" MODIFIED="1724884489352">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Sacred Soil" ID="ID_1512691997" CREATED="1724883644206" MODIFIED="1724883652598"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1 Aetherflow
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="" ID="ID_1647179797" CREATED="1724884489351" MODIFIED="1724884489352">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="-10% Damage Taken" ID="ID_1937985127" CREATED="1724884489353" MODIFIED="1724942704378"/>
+</node>
+<node TEXT="" ID="ID_1258068083" CREATED="1724884462658" MODIFIED="1724884462658">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Fey Illumination" ID="ID_1982431110" CREATED="1724882834584" MODIFIED="1724882849052"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Heal potency +10%
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="" ID="ID_401338009" CREATED="1724884462657" MODIFIED="1724884462658">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Want to use before an attack" ID="ID_1357468941" CREATED="1724884462659" MODIFIED="1724884482635"/>
+</node>
+<node TEXT="" ID="ID_1328561520" CREATED="1724884836941" MODIFIED="1724884836941">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Expedient" ID="ID_964227746" CREATED="1724884824171" MODIFIED="1724884834956"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      SPEEEEEEEEEEEEEDDDDDDD
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="" ID="ID_26114606" CREATED="1724884836940" MODIFIED="1724884836941">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="-10% Damage Taken for 20 seconds" ID="ID_102512288" CREATED="1724884836942" MODIFIED="1724884881222"/>
+</node>
+</node>
+<node TEXT="DefenseAreaGCD" ID="ID_1235272572" CREATED="1724880368614" MODIFIED="1724880379764">
+<node TEXT="Succor" ID="ID_796096028" CREATED="1724882723405" MODIFIED="1724884726169"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      200 Heal | Check party members for status, don't overlap with other Shield healers
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node TEXT="GCD" POSITION="bottom_or_right" ID="ID_959949569" CREATED="1724880190308" MODIFIED="1724880192270">
+<node TEXT="Summon Eos" ID="ID_1657437340" CREATED="1724881294951" MODIFIED="1724881314190"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      lol
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Weaponskill" ID="ID_108356824" CREATED="1724881562684" MODIFIED="1724881570863">
+<node TEXT="Ruin" ID="ID_869616932" CREATED="1724881580345" MODIFIED="1724881581920"/>
+<node TEXT="Bio" ID="ID_609216522" CREATED="1724881588319" MODIFIED="1724881612530"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      DoT
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Ruin II" ID="ID_124303041" CREATED="1724881583418" MODIFIED="1724881619773"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Instant cast attack
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Art of War" ID="ID_1425099455" CREATED="1724881594018" MODIFIED="1724881599251"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      AoE
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node TEXT="oGCD" POSITION="top_or_left" ID="ID_594468070" CREATED="1724880194371" MODIFIED="1724880196653">
+<node TEXT="Utility" ID="ID_1506276164" CREATED="1724881681248" MODIFIED="1724881683501">
+<node TEXT="Chain Stratagem" ID="ID_1353815110" CREATED="1724881683875" MODIFIED="1724881695306"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      2min Burst
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Dissipation" ID="ID_1517937064" CREATED="1724881909216" MODIFIED="1724881952468"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      May use as extra DPS
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Aetherflow" ID="ID_439624980" CREATED="1724883368801" MODIFIED="1724883374645"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Gauge skill
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Deployment Tactics" ID="ID_1355226710" CREATED="1724883756462" MODIFIED="1724883816278"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Recitation -&gt; Adloquium -&gt; Deployment Tactics
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Emergency Tactics" ID="ID_1807052700" CREATED="1724883822178" MODIFIED="1724883854630"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Extra healing! (Rarely used)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Recitation" ID="ID_939606510" CREATED="1724884004983" MODIFIED="1724884022246"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Guaranteed Crit + No MP/Aetherflow use
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Protraction" ID="ID_892067023" CREATED="1724884771288" MODIFIED="1724884812131"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Increases HP of Target and +10% heal potency
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Seraphism" ID="ID_1759571131" CREATED="1724884962035" MODIFIED="1724884996818"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Action change for Adlo and Succ
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="Attack" ID="ID_1009265522" CREATED="1724883381179" MODIFIED="1724883382874">
+<node TEXT="Energy Drain" ID="ID_1660745507" CREATED="1724883383077" MODIFIED="1724883387113"/>
+<node TEXT="Baneful Impaction" ID="ID_121273203" CREATED="1724883937060" MODIFIED="1724883964407"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Used in burst at &gt;=lv92
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node TEXT="Fae Actions" POSITION="bottom_or_right" ID="ID_1514091052" CREATED="1724883162462" MODIFIED="1724883168035">
+<node TEXT="" ID="ID_1523559749" CREATED="1724883429303" MODIFIED="1724883429303">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Fey Illumination" ID="ID_1550314568" CREATED="1724882834584" MODIFIED="1724882849052"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Heal potency +10%
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Fey Blessing" ID="ID_806522394" CREATED="1724883229055" MODIFIED="1724883246782"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      320 Heal
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Whispering Dawn" ID="ID_831236896" CREATED="1724882402668" MODIFIED="1724882414477"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      HoT
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="" ID="ID_1247458578" CREATED="1724883429301" MODIFIED="1724883429302">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Follow use in order unless necessary use for action" ID="ID_1800771060" CREATED="1724883429303" MODIFIED="1724883539097"/>
+</node>
+<node TEXT="" ID="ID_555177686" CREATED="1724884065776" MODIFIED="1724884065776">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Summon Seraph" ID="ID_1712626153" CREATED="1724883287055" MODIFIED="1724883289934">
+<node TEXT="Consolation" ID="ID_939707346" CREATED="1724883291624" MODIFIED="1724883336815"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      250 Heal + Shield
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="" ID="ID_1506988642" CREATED="1724884065775" MODIFIED="1724884065776">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="May use when the 3 above skills are exhausted" ID="ID_1979893442" CREATED="1724884065776" MODIFIED="1724884078611"/>
+</node>
+<node TEXT="Aetherpact" ID="ID_1565317251" CREATED="1724882055218" MODIFIED="1724882096919"><richcontent TYPE="DETAILS">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Slow heal, only use when target &gt;= 85%
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</map>
